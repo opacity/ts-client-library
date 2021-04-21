2aaaf82db09e5e3ab691e54cc3d7de18a7eeb42d
Connor <13987924+CKH4@users.noreply.github.com>
2021-04-20 22:07:25 -0400

    Base64 padding

--- packages/util/src/b64.ts
+++ packages/util/src/b64.ts
@@ -1,7 +1,7 @@
 import { fromUint8Array, toUint8Array } from "js-base64"
 
 export const bytesToB64URL = (b: Uint8Array) => {
-	return fromUint8Array(b, true)
+	return fromUint8Array(b, true).padEnd(Math.ceil(b.length / 4) * 4, "=")
 }
 
 export const b64URLToBytes = (b64: string) => {
