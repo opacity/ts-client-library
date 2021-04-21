4b606ff3d9f92b612e1430bbb57e898f9ae460d2
Connor <13987924+CKH4@users.noreply.github.com>
2021-04-20 22:17:11 -0400

    Update b64.ts

--- packages/util/src/b64.ts
+++ packages/util/src/b64.ts
@@ -1,7 +1,7 @@
 import { fromUint8Array, toUint8Array } from "js-base64"
 
 export const bytesToB64URL = (b: Uint8Array) => {
-	return fromUint8Array(b, true).padEnd(b.length / 3 * 4, "=")
+	return fromUint8Array(b, true).padEnd(Math.ceil(b.length / 3) * 4, "=")
 }
 
 export const b64URLToBytes = (b64: string) => {
